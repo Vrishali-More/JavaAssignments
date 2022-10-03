7d848d260e19bd421117c9edc75a43c05f600626
Vrishali-More <vrishali.more@kpipartners.com>
2022-10-03 17:33:13 +0530

    commit assignment

--- kpi-jnitassignment/src/main/java/com/Question6/Employee.java
+++ kpi-jnitassignment/src/main/java/com/Question6/Employee.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Employee {
-	public static void main(String args[]) {
+	public static String show() {
 
-	Map<String, Integer>emp1 = new HashMap<String , Integer>();
+	Map< Integer, String>emp1 = new HashMap<  Integer , String>();
 	 
 	
-	emp1.put("Sham",1);
-	emp1.put("Ram", 2);
-	emp1.put("Gita",3 );
-	emp1.put("nita",4);
+	emp1.put(1,"Sham");
+	emp1.put(2,"Ram");
+	emp1.put(3,"Gita");
+	emp1.put(4,"nita");
 	
-	System.out.println("HashMap"+emp1);
-	
+	System.out.println( emp1.get(3));
+	return emp1.get(3);
 }
 	
 }
